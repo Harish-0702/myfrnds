f1a0348cbde31b1780bb5eba8d9c732bf6bf136e
Harish T S J <harishtsj@gmail.com>
2024-07-02 15:16:50 +0530

    latest update

--- src/containers/App.js
+++ src/containers/App.js
@@ -37,7 +37,7 @@ class App extends Component {
         else{
             return (
                 <div className="tc">
-                    <h1> My Robo Friends</h1>
+                    <h1 className="app-title">Mechatron UI</h1>
                     <SearchBox searchChange = { this.onSearchChange }/>
                     <Scroll>
                         <CardList Frnds={ filteredFrnds } />
